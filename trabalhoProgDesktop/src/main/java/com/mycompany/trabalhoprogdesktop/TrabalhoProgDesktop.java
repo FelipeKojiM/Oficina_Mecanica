74a488e4b063f2a065fe927c18db312228f57a2a
FelipeKojiM <felipekoji1@outlook.com>
2024-03-27 22:23:52 -0300

    Commit inicial trabalho

---
+++ trabalhoProgDesktop/src/main/java/com/mycompany/trabalhoprogdesktop/TrabalhoProgDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhoprogdesktop;
+
+/**
+ *
+ * @author HP
+ */
+public class TrabalhoProgDesktop {
+
+    public static void main(String[] args) {
+        new TelaLogin().setVisible(true);
+    }
+}
